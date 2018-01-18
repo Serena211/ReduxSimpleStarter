02fe133ab9ac9636936c647ecb8a2d01bab49b8e
Xuanyu Liu <sunflowerxuanyu@gmail.com>
2018-01-18 18:54:37 -0500

    creat components and get youtube API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
+
+const API_KEY = 'AIzaSyBZAnfN4Ma5VqsJDr9QaAPxJGhGK0wnjsk';
 //Create a component that return HTML
-const App =  () => {
-    return <div>Hi</div>
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
 //Render the generated component into the page (in the DOM)
