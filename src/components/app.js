7cebad39ccbcc42139b5a1d7329b281c71dceef9
Xuanyu Liu <sunflowerxuanyu@gmail.com>
2018-01-18 16:31:45 -0500

    add router

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-
+import Headers from './header'
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Headers />
+        {this.props.children}
+      </div>
     );
   }
 }
---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default () => {
+    return (
+        <div>
+            Supper Special Secret Recipe
+            <ul >
+                <li>1 CUP PINEAPPLE</li>
+                <li>1 CUP FLOUR</li>
+                <li>1 CUT WATER</li>
+            </ul>
+        </div>
+    );
+}
